7b96bcd4f72d3d1cc3ee011bcaec5958cd6bad89
RegisM <regis.github.com@tornad.net>
2013-08-23 11:52:06 +0200

    updating graph tools

--- bin/graph.sh
+++ bin/graph.sh
@@ -1,5 +1,6 @@
-FILENAME=/tmp/ulms
-rrdtool graph $FILENAME.png  \
+FILENAME=/tmp/$1.rrd
+rrdtool graph /tmp/$1.png  \
+	--title 'Max value for graph is' \
         --start $(date --date "2013-08-22 15:20:00" +%s) \
         --end $(date --date "2013-08-22 15:30:00" +%s) \
 		--color MGRID#80C080				  \
@@ -8,16 +9,15 @@ rrdtool graph $FILENAME.png  \
 		--color ARROW#FFFFFF				  \
 		--color SHADEA#404040				  \
 		--color SHADEB#404040				  \
-		HRULE:100#444444 \
-		DEF:packetok=$FILENAME.rrd:packetok:LAST  \
-        DEF:latency=$FILENAME.rrd:latency:LAST  \
+	HRULE:100#444444 \
+	DEF:packetok=$FILENAME:packetok:LAST  \
+        DEF:latency=$FILENAME:latency:LAST  \
         CDEF:ok=packetok,99,GT,packetok,0,IF                 \
-		CDEF:nok=packetok,99,GT,0,packetok,IF                  \
-		AREA:ok#33AA33:"OK"               \
-		AREA:nok#FF0000:"NOK"        	\
-		LINE1:latency#0000FF:"Latency"
+	CDEF:nok=packetok,99,GT,0,packetok,IF                  \
+	AREA:ok#33AA33:"OK"               \
+	AREA:nok#FF0000:"NOK"        	\
+	LINE1:latency#0000FF:"Latency"
 
-#		LINE1:packetok#0000FF \
 
-cp $FILENAME.png /vagrant/www
+cp /tmp/$1.png /vagrant/www
 
---
+++ bin/graphAll.sh
@@ -0,0 +1,31 @@
+
+for i in $(ls $1/*.rrd)
+do
+	FILENAME=$(basename $i)
+	
+	FULLNAME=$1/$(basename $i)
+	rrdtool graph $FULLNAME.png  \
+		--title "From $(echo $FILENAME | sed 's/.rrd//') to $(hostname)" \
+	    --start $(date +"%s" -d "1 hour ago") 		\
+	    --end $(date +"%s") 						\
+		--color MGRID#80C080				  		\
+		--color GRID#808020				  			\
+		--color FRAME#808080				  		\
+		--color ARROW#FFFFFF				  		\
+		--color SHADEA#404040				  		\
+		--color SHADEB#404040				  		\
+		HRULE:100#444444 							\
+		DEF:packetok=$FULLNAME:packetok:LAST  		\
+	        DEF:latency=$FULLNAME:latency:LAST  	\
+	        CDEF:ok=packetok,99,GT,packetok,0,IF    \
+		CDEF:nok=packetok,99,GT,0,packetok,IF       \
+		AREA:ok#33AA33:"OK"               			\
+		AREA:nok#FF0000:"NOK"        				\
+		LINE1:latency#0000FF:"Latency"
+
+
+	cp $FULLNAME.png /vagrant/www
+
+done
+
+exit 0
