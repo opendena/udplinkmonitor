f53803541a47326bdb4afd4f32b8ff546a35fd33
RegisM <regis.github.com@tornad.net>
2013-08-26 17:24:46 +0200

    adding HTML generation

--- bin/graphAll.sh
+++ bin/graphAll.sh
@@ -1,34 +1,36 @@
+echo "<html><body>" > $1/index.html
 for j in $(ls $1)
-do
-if [ -d $1/$j ] ; then
-echo $j
-for i in $(ls $1/$j/*.rrd)
-do
-	FILENAME=$(basename $i)
-	
-	FULLNAME=$1/$j/$(basename $i)
-	rrdtool graph $FULLNAME.png  \
-	    --title "From $(echo $FILENAME | sed 's/.rrd//') to $(hostname)" \
-	    --start $(date +"%s" -d "1 hour ago") 		\
-	    --end $(date +"%s") 						\
-		--color MGRID#80C080				  		\
-		--color GRID#808020				  			\
-		--color FRAME#808080				  		\
-		--color ARROW#FFFFFF				  		\
-		--color SHADEA#404040				  		\
-		--color SHADEB#404040				  		\
-		HRULE:100#444444 							\
-		DEF:packetok=$FULLNAME:packetok:LAST  		\
-	        DEF:latency=$FULLNAME:latency:LAST  	\
-	        CDEF:ok=packetok,99,GT,packetok,0,IF    \
-		CDEF:nok=packetok,99,GT,0,packetok,IF       \
-		AREA:ok#33AA33:"OK"               			\
-		AREA:nok#FF0000:"NOK"        				\
-		LINE1:latency#0000FF:"Latency"
+	do
+	if [ -d $1/$j ] ; then
+		echo "<H1>$j</H1>" >> $1/index.html
+		for i in $(ls $1/$j/*.rrd)
+			do
+				FILENAME=$(basename $i)
+				
+				FULLNAME=$1/$j/$(basename $i)
+				rrdtool graph $FULLNAME.png  \
+				    --title "From $(echo $FILENAME | sed 's/.rrd//') to $j" \
+				    --start $(date +"%s" -d "1 hour ago") 		\
+				    --end $(date +"%s") 						\
+					--color MGRID#80C080				  		\
+					--color GRID#808020				  			\
+					--color FRAME#808080				  		\
+					--color ARROW#FFFFFF				  		\
+					--color SHADEA#404040				  		\
+					--color SHADEB#404040				  		\
+					HRULE:100#444444 							\
+					DEF:packetok=$FULLNAME:packetok:LAST  		\
+				        DEF:latency=$FULLNAME:latency:LAST  	\
+				        CDEF:ok=packetok,99,GT,packetok,0,IF    \
+					CDEF:nok=packetok,99,GT,0,packetok,IF       \
+					AREA:ok#33AA33:"OK"               			\
+					AREA:nok#FF0000:"NOK"        				\
+					LINE1:latency#0000FF:"Latency"	> /dev/null
 
-done
+				echo "<img src=\"$j/$FILENAME.png\" /><BR/>" >> $1/index.html
+			done
 
-fi
+	fi
 
-done
+	done
 exit 0
